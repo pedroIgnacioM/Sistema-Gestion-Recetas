afa6bb214eaaea5a6b3a81bb120137f962b8fc46
pedroIgnacioM <pedro.ignaciom95@gmail.com>
2016-10-29 16:29:29 -0300

    probando

--- Proyecto 28_10_2016/src/ClasePrincipal.java
+++ Proyecto 28_10_2016/src/ClasePrincipal.java
@@ -26,7 +26,7 @@
  * 		
  * 
  *********************************************************/
-
+//probando githubjfiewjfqijfqwpjdpqwojfqijfqoijfweoifjeihgaoehwfhsadguoheragoiawhegfoijwaegijwae
 // Crear clase archivo y ver todo lo de archivos (Clase Universidad)
 public class ClasePrincipal 
 {
